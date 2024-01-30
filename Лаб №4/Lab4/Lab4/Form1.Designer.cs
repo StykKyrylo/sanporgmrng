bdb0c1ce408ce62306b7ed472b23976bca0d6a57
StykKyrylo <144342710+StykKyrylo@users.noreply.github.com>
2024-01-30 16:26:18 +0200

    Add files via upload

---
+++ Лаб №4/Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.Copy = new System.Windows.Forms.Button();
+            this.ttt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(410, 29);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(11, 397);
+            this.Add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(129, 29);
+            this.Add.TabIndex = 2;
+            this.Add.Text = "Додати";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(214, 397);
+            this.Delete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(130, 29);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "Видалити";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(428, 397);
+            this.Copy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(136, 29);
+            this.Copy.TabIndex = 4;
+            this.Copy.Text = "Копіювати";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // ttt
+            // 
+            this.ttt.Location = new System.Drawing.Point(637, 397);
+            this.ttt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ttt.Name = "ttt";
+            this.ttt.Size = new System.Drawing.Size(124, 29);
+            this.ttt.TabIndex = 5;
+            this.ttt.Text = "Додати .ttt до асоціації";
+            this.ttt.UseVisualStyleBackColor = true;
+            this.ttt.Click += new System.EventHandler(this.ttt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(858, 437);
+            this.Controls.Add(this.ttt);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button ttt;
+    }
+}
+
---
+++ Лаб №4/Lab4/Lab4/Form1.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void refresh()
+        {
+            RegistryKey k = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey keyMachine = Registry.LocalMachine.OpenSubKey("SOFTWARE").OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion").OpenSubKey("Run", true);
+            RegistryKey keyUser = Registry.CurrentUser.OpenSubKey("SOFTWARE").OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion").OpenSubKey("Run", true);
+            RegistryKey keySchedule = k.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\");
+            string[] machine = keyMachine.GetValueNames();
+            string[] user = keyUser.GetValueNames();
+            string[] schedule = keySchedule.GetSubKeyNames();
+            label1.Text = "Автозагрузка:\n";
+            label2.Text = "Планувальник завдань: \n";
+            for (int i = 0; i < machine.Length; i++)
+                label1.Text += machine[i] + "\n";
+            for (int i = 0; i < user.Length; i++)
+                label1.Text += user[i] + "\n";
+            for (int i = 0; i < schedule.Length; i++)
+                label2.Text += schedule[i] + "\n";
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey keyUser = Registry.CurrentUser.OpenSubKey("SOFTWARE").OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion").OpenSubKey("Run", true);
+            keyUser.SetValue("notepad.exe", @"C:\Windows\system32\notepad.exe");
+            refresh();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            RegistryKey keyUser = Registry.CurrentUser.OpenSubKey("SOFTWARE").OpenSubKey("Microsoft").OpenSubKey("Windows").OpenSubKey("CurrentVersion").OpenSubKey("Run", true);
+            keyUser.DeleteValue("notepad.exe");
+            refresh();
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\kp611\Desktop\copy.reg";
+            string key = @"HKEY_CURRENT_USER";
+            Process proc = new Process();
+            try
+            {
+                proc.StartInfo.FileName = "regedit.exe";
+                proc.StartInfo.UseShellExecute = false;
+                proc = Process.Start("regedit.exe", "/e " + path + " " + key);
+                proc.WaitForExit();
+            }
+            catch (Exception)
+            {
+                proc.Dispose();
+            }
+        }
+
+        private void ttt_Click(object sender, EventArgs e)
+        {
+            var path = $@"C:\Users\kp611\Desktop\ttt.reg";
+            string arguments = $"/s {path}";
+            var regeditProcess = Process.Start($"regedit.exe", arguments);
+            regeditProcess.WaitForExit();
+        }
+    }
+}
